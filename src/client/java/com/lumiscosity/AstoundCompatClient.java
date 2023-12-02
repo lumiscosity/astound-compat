4b0bbee7e4c7133df0f3801f8db79e9f11c8e490
lumiscosity <averyrudelphe@gmail.com>
2023-12-02 20:33:25 +0100

    add hand models because minecraft doesn't have defaults, add most of the acai ladder

---
+++ src/client/java/com/lumiscosity/AstoundCompatClient.java
@@ -0,0 +1,13 @@
+package com.lumiscosity;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+
+public class AstoundCompatClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        BlockRenderLayerMap.INSTANCE.putBlock(AstoundCompat.GREAT_BIG_WORLD_ACAI_LADDER, RenderLayer.getCutout());
+    }
+}
--- src/main/java/com/lumiscosity/AstoundCompat.java
+++ src/main/java/com/lumiscosity/AstoundCompat.java
@@ -2,16 +2,33 @@ package com.lumiscosity;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.LadderBlock;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.*;
+import net.minecraft.item.BlockItem;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static net.minecraft.block.Blocks.LADDER;
+
 public class AstoundCompat implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("astound_compat");
+	public static final Block GREAT_BIG_WORLD_ACAI_LADDER = new LadderBlock(FabricBlockSettings.copy(LADDER).sounds(BlockSoundGroup.CHERRY_WOOD));
 	@Override
 	public void onInitialize() {
-		public static final Block GREAT_BIG_WORLD_ACAI_LADDER = register("great_big_world_acai_ladder", new LadderBlock(FabricBlockSettings.copy(LADDER).sounds(BlockSoundGroup.CHERRY_WOOD)));
+
+		makeBlock("great_big_world_acai_ladder", GREAT_BIG_WORLD_ACAI_LADDER);
+
 		LOGGER.info("Astound compatibility tweaks loaded!");
+	}
+
+	private void makeBlock(String id, Block block) {
+		Registry.register(Registries.BLOCK, new Identifier("astound", id), block);
+		Registry.register(Registries.ITEM, new Identifier("astound", id), new BlockItem(block, new FabricItemSettings()));
 	}
 }
