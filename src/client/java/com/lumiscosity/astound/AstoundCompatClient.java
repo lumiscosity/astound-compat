874aa991f3f7c5efe7984d00cd11d2b353377b01
lumiscosity <averyrudelphe@gmail.com>
2023-12-03 16:11:51 +0100

    add final data, prep for hollowed logs

--- src/client/java/com/lumiscosity/astound/AstoundCompatClient.java
+++ src/client/java/com/lumiscosity/astound/AstoundCompatClient.java
@@ -1,11 +1,10 @@
-package com.lumiscosity;
+package com.lumiscosity.astound;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.RenderLayer;
 
 public class AstoundCompatClient implements ClientModInitializer {
-
     @Override
     public void onInitializeClient() {
         BlockRenderLayerMap.INSTANCE.putBlock(AstoundCompat.GREAT_BIG_WORLD_ACAI_LADDER, RenderLayer.getCutout());
--- src/main/java/com/lumiscosity/astound/AstoundCompat.java
+++ src/main/java/com/lumiscosity/astound/AstoundCompat.java
@@ -1,4 +1,4 @@
-package com.lumiscosity;
+package com.lumiscosity.astound;
 
 import net.fabricmc.api.ModInitializer;
 
@@ -11,18 +11,29 @@ import net.minecraft.registry.Registry;
 import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Direction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static net.minecraft.block.Blocks.BOOKSHELF;
 import static net.minecraft.block.Blocks.LADDER;
 
 public class AstoundCompat implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("astound_compat");
+	// hollowed log (uses ww native code for compat)
+	// public static final HollowedLogBlock GREAT_BIG_WORLD_HOLLOWED_ACAI_LOG = createHollowedLogBlock(MapColor.BROWN, MapColor.TERRACOTTA_BROWN);
+	// public static final HollowedLogBlock STRIPPED_HOLLOWED_OAK_LOG = createStrippedHollowedLogBlock(MapColor.BROWN, MapColor.TERRACOTTA_BROWN);
+
+	// ladder (also needs a render type set in the client)
 	public static final Block GREAT_BIG_WORLD_ACAI_LADDER = new LadderBlock(FabricBlockSettings.copy(LADDER).sounds(BlockSoundGroup.CHERRY_WOOD));
+	// bookshelf
+	public static final Block GREAT_BIG_WORLD_ACAI_BOOKSHELF = new Block(FabricBlockSettings.copy(BOOKSHELF).sounds(BlockSoundGroup.CHISELED_BOOKSHELF));
 	@Override
 	public void onInitialize() {
 
 		makeBlock("great_big_world_acai_ladder", GREAT_BIG_WORLD_ACAI_LADDER);
+
+		makeBlock("great_big_world_acai_bookshelf", GREAT_BIG_WORLD_ACAI_BOOKSHELF);
 
 		LOGGER.info("Astound compatibility tweaks loaded!");
 	}
