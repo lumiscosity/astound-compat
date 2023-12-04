b5c74cd097e3a5896b103e10c1e2e7cee5158225
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2023-12-03 21:58:55 -0500

    Register two Hollowed Logs

--- src/main/java/com/lumiscosity/astound/HollowedLogCompat.java
+++ src/main/java/com/lumiscosity/astound/HollowedLogCompat.java
@@ -1,57 +1,30 @@
 package com.lumiscosity.astound;
 
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import com.lumiscosity.astound.mixin.HollowedLogInvoker;
 import net.frozenblock.wilderwild.block.HollowedLogBlock;
 import net.frozenblock.wilderwild.entity.ai.TermiteManager.Termite;
-import net.frozenblock.wilderwild.registry.RegisterBlockSoundTypes;
+import net.minecraft.block.Block;
 import net.minecraft.block.MapColor;
-import net.minecraft.block.enums.Instrument;
-import net.minecraft.sound.BlockSoundGroup;
-import net.minecraft.util.math.Direction;
-import org.jetbrains.annotations.NotNull;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public final class HollowedLogCompat{
-    // public static final HollowedLogBlock GREAT_BIG_WORLD_HOLLOWED_ACAI_LOG = createHollowedLogBlock(MapColor.BROWN, MapColor.TERRACOTTA_BROWN);
-    // public static final HollowedLogBlock GREAT_BIG_WORLD_STRIPPED_HOLLOWED_ACAI_LOG = createStrippedHollowedLogBlock(MapColor.BROWN);
-    public static void create_and_register() {
+    public static final HollowedLogBlock GREAT_BIG_WORLD_HOLLOWED_ACAI_LOG = HollowedLogInvoker.invokeCreateHollowedLogBlock(MapColor.BROWN, MapColor.TERRACOTTA_BROWN);
+    public static final HollowedLogBlock GREAT_BIG_WORLD_STRIPPED_HOLLOWED_ACAI_LOG = HollowedLogInvoker.invokeCreateStrippedHollowedLogBlock(MapColor.BROWN);
+
+    public static void createAndRegister() {
+        register("great_big_world_hollowed_acai_log", GREAT_BIG_WORLD_HOLLOWED_ACAI_LOG);
+        register("great_big_world_stripped_hollowed_acai_log", GREAT_BIG_WORLD_STRIPPED_HOLLOWED_ACAI_LOG);
         registerTermite();
     }
+
     private static void registerTermite() {
-        // Termite.addDegradable(GREAT_BIG_WORLD_HOLLOWED_ACAI_LOG, GREAT_BIG_WORLD_STRIPPED_HOLLOWED_ACAI_LOG);
-    }
-    // these are private, so we have to "reimplement" them
-    private static @NotNull HollowedLogBlock createHollowedLogBlock(MapColor topMapColor, MapColor sideMapColor, BlockSoundGroup soundType) {
-        FabricBlockSettings settings = FabricBlockSettings.create().mapColor((state) -> {
-            return state.get(HollowedLogBlock.AXIS) == Direction.Axis.Y ? topMapColor : sideMapColor;
-        }).instrument(Instrument.BASS).strength(2.0F).sounds(soundType).burnable();
-        return new HollowedLogBlock(settings);
+        Termite.addDegradable(GREAT_BIG_WORLD_HOLLOWED_ACAI_LOG, GREAT_BIG_WORLD_STRIPPED_HOLLOWED_ACAI_LOG);
     }
 
-    private static @NotNull HollowedLogBlock createHollowedLogBlock(MapColor topMapColor, MapColor sideMapColor) {
-        return createHollowedLogBlock(topMapColor, sideMapColor, RegisterBlockSoundTypes.HOLLOWED_LOG);
-    }
-
-    private static @NotNull HollowedLogBlock createHollowedStemBlock(MapColor mapColor) {
-        return new HollowedLogBlock(FabricBlockSettings.create().mapColor((state) -> {
-            return mapColor;
-        }).instrument(Instrument.BASS).strength(2.0F).sounds(RegisterBlockSoundTypes.HOLLOWED_STEM));
-    }
-
-    private static @NotNull HollowedLogBlock createStrippedHollowedLogBlock(MapColor mapColor, BlockSoundGroup soundType) {
-        FabricBlockSettings settings = FabricBlockSettings.create().mapColor((state) -> {
-            return mapColor;
-        }).instrument(Instrument.BASS).strength(2.0F).sounds(soundType).burnable();
-        return new HollowedLogBlock(settings);
-    }
-
-    private static @NotNull HollowedLogBlock createStrippedHollowedLogBlock(MapColor mapColor) {
-        return createStrippedHollowedLogBlock(mapColor, RegisterBlockSoundTypes.HOLLOWED_LOG);
-    }
-
-    private static @NotNull HollowedLogBlock createStrippedHollowedStemBlock(MapColor mapColor) {
-        return new HollowedLogBlock(FabricBlockSettings.create().mapColor((state) -> {
-            return mapColor;
-        }).instrument(Instrument.BASS).strength(2.0F).sounds(RegisterBlockSoundTypes.HOLLOWED_STEM));
+    private static void register(String id, Block block) {
+        Registry.register(Registries.BLOCK, new Identifier("astound", id), block);
     }
 
 }
